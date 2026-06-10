1e37dce569f12b35b1817e92f939511d5d6d6676
samsungsr2026 <chshik@naver.com>
2026-06-10 13:45:29 +0900

    fix: sw.js 캐시 전체 삭제 - 구버전 강제 갱신

--- sw.js
+++ sw.js
@@ -1,3 +1,17 @@
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>clients.claim());
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const CACHE_VER = 'sr-v202606100445';
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  // 항상 네트워크 우선, 실패시에만 캐시
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
